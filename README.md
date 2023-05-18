8b2930471a444769564da9fc0ff76925ec94bde3
EranGrin <soniceran@gmail.com>
2023-05-18 09:27:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# webpack-5-react-starter-zsn6gy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/webpack-5-react-starter-zsn6gy)
---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div
+     class="bg-yellow-300" 
+    >
+        <div>
+            <router-view
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Main',
+    props: {
+        apiToken: { type: String, required: true },
+        baseUri: { type: String, default: 'test.me' },
+    },
+
+    data: () => ({
+        configuration: {},
+        credentials: {
+            apiToken: '',
+            baseUri: '',
+        },
+    }),
+
+    created() {
+        console.log(this);
+    },
+
+};
+</script>
+<style >
+.test-heading {
+    font-size: 2em;
+    margin-top: 2rem;
+}
+</style>
+
